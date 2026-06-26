3c5614c6b61e783823e918084b07496134f157c1
mjf1406 <michael.fitzgerald.1406@gmail.com>
2026-06-26 20:04:57 +0900

    feat: Convex, shadcn preset, and Tanstack Router

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# classclarus-convex
+# ClassClarus
+
+## Useful Links
+
+- [Shadcn Preset](https://ui.shadcn.com/create?preset=b5wPv7go4&template=vite&pointer=true)
+
+## To Do
+
+- Convex Auth working
+- light/dark theme toggle working
+- created `.env.local.example` file
+- set up light/dark sonner colors for success, warning, error, and info
+- add a minimal shadcn sidebar app layout shell with breadcrumbs in the header
+- 404 route with a link back to the home page
+- error boundary route with a link back to the home page
+- dev tools behind env for only in dev
+- somehow make Convex queries automatically optimistic
+
+## Change Log
+
+### 2026-06-26
+
+- installed and initialized Convex
+- installed and initialized shadcn preset
+- installed and initialized Tanstack Router
---
+++ src/routes/__root.tsx
@@ -0,0 +1,32 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import '../styles.css'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <>
+      <ConvexProvider client={convex}>
+        <Outlet />
+      </ConvexProvider>
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'TanStack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  )
+}
